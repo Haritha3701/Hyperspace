111b3a40c800666ffc0eb4342ff57c0e1e95414c
Haritha <harithaks3701@gmail.com>
2023-03-09 16:49:21 +0530

    Single page loading,get product details when clicked on products

--- hyperspace/product/urls.py
+++ hyperspace/product/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("abc/",views.test),
+    path("",views.details),
+
 ]
--- hyperspace/product/views.py
+++ hyperspace/product/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import plans
 
 # Create your views here.
 
 def test(request):
     return render(request,"test.html")
+
+def details(request):
+    id=request.GET["id"]
+    data=plans.objects.get(id=id)
+    return render(request,"details.html",{"data":data})
